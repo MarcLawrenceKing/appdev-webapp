7dd75d4e64b4cce12277b8d74cd3a3947565fddd
Marc King <marclawrenceking@gmail.com>
2025-05-30 14:28:55 +0800

    fixed issue with canot be found route CreateEditExpenseForm

--- appdev-webapp/Controllers/HomeController.cs
+++ appdev-webapp/Controllers/HomeController.cs
@@ -20,6 +20,51 @@ namespace appdev_webapp.Controllers
         {
             return View();
         }
+        // GET: Home/CreateEditExpenseForm/5 or Home/CreateEditExpenseForm
+        public IActionResult CreateEditExpense(int? id)
+        {
+            if (id == null)
+                return View(new Expense()); // No ID provided -> Show blank form for creating
+
+            var expense = _context.Expenses.Find(id);
+            if (expense == null)
+                return NotFound(); // ID provided but not found in DB
+
+            return View(expense); // Load existing expense -> Show in edit mode
+        }
+
+
+        // save the form data, either create or update a new record
+        [HttpPost]
+        public IActionResult CreateEditExpense(Expense model)
+        {
+            if (model.Id == 0)
+            {
+                _context.Expenses.Add(model); // New expense -> Add to DB
+            }
+            else
+            {
+                _context.Expenses.Update(model); // Existing expense -> Update in DB
+            }
+
+            _context.SaveChanges(); // Save to DB
+            return RedirectToAction("Expense"); // Go back to list view
+        }
+
+
+
+        // POST: /Expense/Delete/5
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var expense = _context.Expenses.Find(id);
+            if (expense == null) return NotFound();
+
+            _context.Expenses.Remove(expense);
+            _context.SaveChanges();
+
+            return RedirectToAction("Expense");
+        }
 
         public IActionResult Expense()
         {
@@ -27,18 +72,18 @@ namespace appdev_webapp.Controllers
             return View(allExpenses);
         }
 
-        public IActionResult CreateEditExpense()
-        {
-            return View();
-        }
+        //public IActionResult CreateEditExpense()
+        //{
+        //    return View();
+        //}
 
-        public IActionResult CreateEditExpenseForm(Expense model)
-        {
-            _context.Expenses.Add(model);
-            _context.SaveChanges();
+        //public IActionResult CreateEditExpenseForm(Expense model)
+        //{
+        //    _context.Expenses.Add(model);
+        //    _context.SaveChanges();
 
-            return RedirectToAction("Expense");
-        }
+        //    return RedirectToAction("Expense");
+        //}
 
         public IActionResult Privacy()
         {
